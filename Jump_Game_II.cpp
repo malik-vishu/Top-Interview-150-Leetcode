33ade962e34d999c0d038a244eb92c93c8fe0ea6
Vishwas Malik <127117253+malik-vishu@users.noreply.github.com>
2023-09-03 15:14:13 +0530

    Create Jump_Game_II.cpp

---
+++ Jump_Game_II.cpp
@@ -0,0 +1,16 @@
+//45. Jump Game II
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int curr = 0,far = 0;
+        for(int i = 0;i<nums.size()-1;i++){
+            far = max(far,nums[i]+i);
+            if(curr == i){
+                curr = far;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+};
